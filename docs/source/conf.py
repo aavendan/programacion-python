25f01bc192b96fd54d82a15075373d33b46a9462
aavendan <aavenda@gmail.com>
2025-02-05 19:31:29 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Programación con Python'
+copyright = '2025, Allan Avendaño'
+author = 'Allan Avendaño'
 
 release = '0.1'
 version = '0.1.0'
